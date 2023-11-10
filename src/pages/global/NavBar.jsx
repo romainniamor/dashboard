bf18e7620897bc974dff612fd46dbcc8d01d1916
romainniamor <romainnavoret@gmail.com>
2023-11-10 18:21:55 +0100

    setup theme and toogle theme

---
+++ src/pages/global/NavBar.jsx
@@ -0,0 +1,49 @@
+import { Box, IconButton, useTheme } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "../../theme";
+import InputBase from "@mui/material/InputBase";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+
+const NavBar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+
+  return (
+    <Box display={"flex"} justifyContent={"space-between"} p={2}>
+      <Box display={"flex"} bgcolor={colors.primary[400]} borderRadius={3}>
+        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+        <IconButton type="button" sx={{ p: 1 }}>
+          <SearchOutlinedIcon />
+        </IconButton>
+      </Box>
+      <Box display={"flex"}>
+        <IconButton>
+          <PersonOutlineOutlinedIcon />
+        </IconButton>
+
+        <IconButton>
+          <SearchOutlinedIcon />
+        </IconButton>
+
+        <IconButton>
+          <SettingsOutlinedIcon />
+        </IconButton>
+
+        <IconButton onClick={colorMode.toggleColorMode}>
+          {theme.palette.mode === "dark" ? (
+            <LightModeOutlinedIcon />
+          ) : (
+            <DarkModeOutlinedIcon />
+          )}
+        </IconButton>
+      </Box>
+    </Box>
+  );
+};
+
+export default NavBar;
